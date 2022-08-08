aa92409b11d69d7e720d1f374130111cfd0dca83
Akash2811000 <akashgupta7383@gmail.com>
2022-08-08 16:30:36 +0530

    payment_with_rozopay

---
+++ BackendFinal/Backend/src/domain/payment_domain.ts
@@ -0,0 +1,76 @@
+import express, { Express, Request, Response } from 'express';
+import * as dotenv from 'dotenv';
+dotenv.config();
+import { Razorpay } from 'razorpay-typescript';
+import * as crypto from "crypto";
+import { StatusCode } from '../statuscode';
+// import {RazorWebhook} from 'razorpay-typescript'
+// import { Razorpay } from "../dist/razorpay";
+//import Razorpay from 'razorpay';
+var secretkey = process.env.secretkey;
+const instance: Razorpay = new Razorpay({
+    authKey: {
+        key_id: 'rzp_test_LqRkyAt6cQpVui',
+        key_secret: 'yCKG9zsdIoWft58QwrYxjf1G',
+    },
+});
+const secret_key = 'yCKG9zsdIoWft58QwrYxjf1G'
+class PaymentDomain {
+
+    async createOrder(req: Request, res: Response) {
+        let amount = req.body.amount;
+        const options = {
+            amount: amount,
+            currency: 'INR',
+            // receipt: "order_rcptid_11", // any unique id
+        }
+        console.log(options);
+        try {
+
+
+            const response = await instance.orders.create(options);
+            console.log("hy");
+            console.log(response.currency);
+            res.json({
+                order_id: response.id,
+                currency: response.currency,
+                amount: response.amount
+            })
+        } catch (error: any) {
+            res.status(400).send('Unable to create order');
+        }
+
+
+
+    }
+
+    async verifypayment(req: Request, res: Response) {
+        // const data = crypto.createHmac('sha256', secret_key)
+
+        // data.update(JSON.stringify(req.body))
+        // const digest = data.digest('hex')
+        // if (digest === req.headers['x-razorpay-signature']) {
+        //     console.log('request is legit')
+        //     //we can store detail in db and send the response
+        //     res.json({
+        //         status: 'ok'
+        //     })
+        // } else {
+        //     res.status(400).send('Invalid signature');
+        // }
+        var orderId = req.body.orderId;
+        var paymentId = req.body.paymentId;
+        var body = orderId + "|" + paymentId;
+        var expectedSignature = crypto.createHmac('sha256',secret_key).update(body.toString()).digest("hex");
+        console.log("sig" , req.body.razorpay_signature);
+        console.log("sig" , req.body.expectedSignature);
+        var response = {status : "failure"};
+        if(expectedSignature === req.body.razorpay_signature){
+            response = {status : "sucess"};
+            res.send(response);
+        }
+
+    }
+}
+
+export { PaymentDomain };
